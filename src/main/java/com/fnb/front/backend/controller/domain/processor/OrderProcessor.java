4bab976f70f90182014af39cd9b2db894a654303
이두희 <clampea4120@gmail.com>
2025-10-22 23:05:09 +0900

    반복되는 계산로직 개선

--- src/main/java/com/fnb/front/backend/controller/domain/processor/OrderProcessor.java
+++ src/main/java/com/fnb/front/backend/controller/domain/processor/OrderProcessor.java
@@ -50,10 +50,11 @@ public class OrderProcessor {
             throw new IllegalStateException("구매 불가능한 상품이 포함되어 있습니다.");
         }
 
-        String orderId              = CommonUtil.generateOrderId();
-        int totalCouponPrice        = this.calcTotalCouponPrice(this.products, this.coupons);
-        int totalMemberShipPrice    = this.calcTotalMemberShipPrice(this.member, this.products);
-        int totalOriginPrice        = this.calcTotalOriginPrice(this.products);
+        String orderId = CommonUtil.generateOrderId();
+        List<CreateOrderProductDto> orderProductsDto = this.buildOrderProducts(orderId, this.member, this.products, this.coupons);
+        int totalCouponPrice        = orderProductsDto.stream().mapToInt(CreateOrderProductDto::getCouponPrice).sum();
+        int totalMemberShipPrice    = orderProductsDto.stream().mapToInt(CreateOrderProductDto::getMemberShipPrice).sum();
+        int totalOriginPrice        = orderProductsDto.stream().mapToInt(CreateOrderProductDto::getOriginPrice).sum();
 
         return CreateOrderDto.builder()
                 .orderId(orderId)
@@ -62,7 +63,7 @@ public class OrderProcessor {
                 .discountAmount(totalCouponPrice + totalMemberShipPrice + this.order.getUsePoint().intValue())
                 .couponAmount(totalCouponPrice)
                 .orderAmount(totalOriginPrice)
-                .orderProducts(this.buildOrderProducts(orderId, this.member, this.products, this.coupons))
+                .orderProducts(orderProductsDto)
                 .build();
     }
 
@@ -136,41 +137,6 @@ public class OrderProcessor {
                                 product.getApplyMemberGradeDisAmt().intValue(), memberShipPolicy);
 
             price = memberShipCalculator.calculate().intValue();
-        }
-
-        return price;
-    }
-
-    private int calcTotalCouponPrice(List<Product> products, List<Coupon> coupons) {
-        int price = 0;
-
-        for (Product product : products) {
-            price += this.calcCouponPriceToProduct(product, coupons);
-        }
-
-        return price;
-    }
-
-    private int calcTotalMemberShipPrice(Member member, List<Product> products) {
-        int price = 0;
-
-        for (Product product : products) {
-            if (product.inMemberShipDiscount(member)) {
-                price += this.calcMemberShipPriceToProduct(product, member);
-            }
-        }
-
-        return price;
-    }
-
-    private int calcTotalOriginPrice(List<Product> products) {
-        int price = 0;
-
-        for (Product product : products) {
-            List<ProductOption> productOptions = product.getProductOption();
-            int basicOptionWithPrice = product.getPrice().intValue() + this.getBasicOptionPrice(productOptions);
-            price += (this.calcPriceWithQuantity(basicOptionWithPrice, product.getQuantity())
-                                        + this.getTotalAddOptionPrice(productOptions));
         }
 
         return price;
